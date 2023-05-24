049e651f166e042e998d411e1595fb2668506b9a
Alvaro Alonso <alvaroaq@outlook.es>
2023-05-24 16:44:59 +0200

    Login y registro solo esteticos

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,10 +1,63 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableHighlight,
+  Linking,
+  ImageBackground,
+} from "react-native";
+
 class LoginScreen extends Component {
   render() {
+    const { navigation } = this.props;
     return (
       <View style={styles.container}>
-        <Text style={styles.textTitle}>LoginScreen</Text>
+        <View style={styles.headerView}>
+          <ImageBackground
+            style={{ height: "100%", width: "100%" }}
+            source={require("../assets/header.jpg")}
+          >
+            <View style={styles.headerContainer}>
+              <Text style={styles.header}>Login</Text>
+            </View>
+          </ImageBackground>
+        </View>
+        <View style={styles.form}>
+          <View style={styles.inputView}>
+            <Image
+              style={styles.inputIcon}
+              source={require("../assets/nuk-icons/png/2x/single-02@2x.png")}
+            />
+            <TextInput placeholder="Usuario" placeholderTextColor={"#898b8e"} />
+          </View>
+          <View style={styles.inputView}>
+            <Image
+              style={styles.inputIcon}
+              source={require("../assets/nuk-icons/png/2x/key-25@2x.png")}
+            />
+            <TextInput
+              placeholder="Contraseña"
+              placeholderTextColor={"#898b8e"}
+              secureTextEntry={true}
+            />
+          </View>
+          <TouchableHighlight style={styles.inputSubmit}>
+            <Text style={{ color: "white" }}>Iniciar Sesión</Text>
+          </TouchableHighlight>
+
+          <View style={{ flexDirection: "row" }}>
+            <Text>¿Todavía no tienes cuenta?</Text>
+            <Text
+              style={{ color: "blue", textDecorationLine: "underline" }}
+              onPress={() => navigation.navigate("Registro")}
+            >
+              Regístrate
+            </Text>
+          </View>
+        </View>
       </View>
     );
   }
@@ -13,13 +66,73 @@ class LoginScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#f2f2f2",
+    alignItems: "center",
+  },
+  form: {
+    paddingVertical: "20%",
+
+    width: "100%",
+
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  headerView: {
+    width: "100%",
+    height: 220,
+
+    alignItems: "center",
+    zIndex: 50,
+  },
+  headerContainer: {
+    width: 120,
+    height: 120,
+    borderRadius: 120 / 2,
+    backgroundColor: "#444c6d",
+
     alignItems: "center",
     justifyContent: "center",
+
+    position: "relative",
+
+    top: 160,
+    alignSelf: "center",
   },
-  textTitle: {
-    flex: 1,
-    fontSize: 24,
-    fontWeight: "bold",
+  header: {
+    color: "white",
+    fontSize: 20,
+  },
+  inputView: {
+    marginVertical: 10,
+
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    borderRadius: 100 / 2,
+
+    width: "80%",
+
+    backgroundColor: "#e6e6e6",
+
+    flexDirection: "row",
+  },
+  inputSubmit: {
+    marginVertical: 10,
+
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    borderRadius: 100 / 2,
+
+    width: "80%",
+
+    backgroundColor: "#444c6d",
+
+    alignItems: "center",
+  },
+  inputIcon: {
+    height: 25,
+    width: 25,
+
+    marginHorizontal: 9,
   },
 });
 
--- screens/RegistroScreen.js
+++ screens/RegistroScreen.js
@@ -1,26 +1,162 @@
 import React, { Component } from "react";
-import {View,Text,StyleSheet} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableHighlight,
+  Linking,
+  ImageBackground,
+} from "react-native";
 
 class RegistroScreen extends Component {
   render() {
+    const { navigation } = this.props;
     return (
       <View style={styles.container}>
-        <Text style={styles.textTitle}>RegistroScreen</Text>
+        <View style={styles.headerView}>
+          <ImageBackground
+            style={{ height: "100%", width: "100%" }}
+            source={require("../assets/header.jpg")}
+          >
+            <View style={styles.headerContainer}>
+              <Text style={styles.header}>Registro</Text>
+            </View>
+          </ImageBackground>
+        </View>
+        <View style={styles.form}>
+          <View style={styles.inputView}>
+            <Image
+              style={styles.inputIcon}
+              source={require("../assets/nuk-icons/png/2x/email-85@2x.png")}
+            />
+            <TextInput
+              placeholder="Email"
+              placeholderTextColor={"#898b8e"}
+              inputMode="email"
+              keyboardType="email-address"
+            />
+          </View>
+          <View style={styles.inputView}>
+            <Image
+              style={styles.inputIcon}
+              source={require("../assets/nuk-icons/png/2x/single-02@2x.png")}
+            />
+            <TextInput placeholder="Usuario" placeholderTextColor={"#898b8e"} />
+          </View>
+          <View style={styles.inputView}>
+            <Image
+              style={styles.inputIcon}
+              source={require("../assets/nuk-icons/png/2x/key-25@2x.png")}
+            />
+            <TextInput
+              placeholder="Contraseña"
+              placeholderTextColor={"#898b8e"}
+              secureTextEntry={true}
+            />
+          </View>
+          <View style={styles.inputView}>
+            <Image
+              style={styles.inputIcon}
+              source={require("../assets/nuk-icons/png/2x/key-25@2x.png")}
+            />
+            <TextInput
+              placeholder="Repite la contraseña"
+              placeholderTextColor={"#898b8e"}
+              secureTextEntry={true}
+            />
+          </View>
+          <TouchableHighlight style={styles.inputSubmit}>
+            <Text style={{ color: "white" }}>Crear Cuenta</Text>
+          </TouchableHighlight>
+
+          <View style={{ flexDirection: "row" }}>
+            <Text>¿Ya tienes cuenta?</Text>
+            <Text
+              style={{ color: "blue", textDecorationLine: "underline" }}
+              onPress={() => navigation.navigate("Login")}
+            >
+              Inicia Sesión
+            </Text>
+          </View>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    textTitle: {
-      flex: 1,
-      fontSize: 24,
-      fontWeight: "bold",
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "#f2f2f2",
+    alignItems: "center",
+  },
+  form: {
+    paddingVertical: "20%",
+
+    width: "100%",
+
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  headerView: {
+    width: "100%",
+    height: 220,
+
+    alignItems: "center",
+    zIndex: 50,
+  },
+  headerContainer: {
+    width: 120,
+    height: 120,
+    borderRadius: 120 / 2,
+    backgroundColor: "#444c6d",
+
+    alignItems: "center",
+    justifyContent: "center",
+
+    position: "relative",
+
+    top: 160,
+    alignSelf: "center",
+  },
+  header: {
+    color: "white",
+    fontSize: 20,
+  },
+  inputView: {
+    marginVertical: 10,
+
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    borderRadius: 100 / 2,
+
+    width: "80%",
+
+    backgroundColor: "#e6e6e6",
+
+    flexDirection: "row",
+  },
+  inputSubmit: {
+    marginVertical: 10,
+
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    borderRadius: 100 / 2,
+
+    width: "80%",
+
+    backgroundColor: "#444c6d",
+
+    alignItems: "center",
+  },
+  inputIcon: {
+    height: 25,
+    width: 25,
+
+    marginHorizontal: 9,
+  },
+});
+
 export default RegistroScreen;
